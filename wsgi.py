a5ba2435fb64bbb073836dbe7e1f9bb35073fc20
Pavlo Marii <pavlomarii@gmail.com>
2021-05-28 18:12:26 +0300

    Configured Sqlite Database

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,12 @@
+import config
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object(config.Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 @app.route('/')
